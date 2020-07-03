5175f67b0a1191162a03c40a0d3ffa2b2b370a14
loket <loket@cruftlab.io>
2020-07-03 17:21:41 +0200

    feat: sort pages by priority
    fix: folder structure
    
    fixes #7

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,25 @@
 module.exports = function (config) {
     // Layouts
-    config.addLayoutAlias('base', 'base.njk');
+    config.addLayoutAlias('base', 'base.njk')
 
     // Copy CSS
-    config.addPassthroughCopy("src/static");
+    config.addPassthroughCopy("src/static")
 
     // Add stats filters
     config.addFilter("count", function(collection) {
-        return collection.length;
-    });
+        return collection.length
+    })
     config.addFilter("combinations", function(collection) {
-        return collection.length * (collection.length - 1);
-    });
+        return collection.length * (collection.length - 1)
+    })
     config.addFilter("quote", function(collection) {
         return collection.map(w => `"${w}"`)
-    });
+    })
+
+    // Add sorting
+    config.addFilter("sortedByPriority", function(collection) {
+        return collection.sort((a, b) => a["data"]["priority"] - b["data"]["priority"])
+    })
 
     return {
         dir: {
@@ -29,4 +34,4 @@ module.exports = function (config) {
         markdownTemplateEngine: 'njk',
         passthroughFileCopy: true
     }
-};
+}
